95d4b1e0f2952a502bc50e70bdb827ede274d35f
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-11 20:23:19 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -5,9 +5,14 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+import { GenerarValorCarta, GenerarCartaHTML } from "./funciones";
+
 window.onload = function() {
-  let cartasJugador = [],
+  let valoresCartas = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
+    cartasJugador = [],
     cartasComputadora = [],
     simbolos = ["♦", "♥", "♠", "♣"],
     simbolosEspeciales = ["K", "Q"];
+
+  GenerarCartaHTML(GenerarValorCarta(valoresCartas, simbolos));
 };
--- src/funciones.js
+++ src/funciones.js
@@ -1,5 +1,45 @@
-const NumeroAleatorio = (arr) => {
-    let randomNumber = Math.floor(Math.random() * arr.le);
-}
+import { arrow } from "@popperjs/core";
 
-const GenerarCarta = () => {};
+const NumeroAleatorio = arr => {
+  let randomNumber = Math.floor(Math.random() * arr.length);
+  return randomNumber;
+};
+
+export const GenerarValorCarta = (valores, simbolos) => {
+  let valorAleatorio = valores[NumeroAleatorio(valores)];
+
+  let carta =
+    typeof valorAleatorio === "number"
+      ? (carta = [
+          `${valorAleatorio}`,
+          `${simbolos[NumeroAleatorio(simbolos)]}`
+        ])
+      : (carta = [`${valorAleatorio}`, `${valorAleatorio}`]);
+  return carta;
+};
+
+export const GenerarCartaHTML = arr => {
+  const cartas = document.querySelectorAll(".carta");
+
+  cartas.forEach(function(carta) {
+    carta.innerHTML = ` <div class="row">
+                <div
+                  class="col-12 contenedor-carta rounded-3 bg-white text-danger"
+                >
+                  <div class="row">
+                    <div class="col-12">
+                      <span>♣</span>
+                    </div>
+                  </div>
+                  <div class="row contenedorNumero">
+                    <div class="col-12">8</div>
+                  </div>
+                  <div class="row">
+                    <div class="col-12 icono-abajo">
+                      <span>♣</span>
+                    </div>
+                  </div>
+                </div>
+              </div>`;
+  });
+};
