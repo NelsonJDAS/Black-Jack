f700524423aa201d6b086ed2f0afc582137af956
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-10-12 18:35:46 +0000

    Functions

--- src/app.js
+++ src/app.js
@@ -5,14 +5,62 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-import { GenerarValorCarta, GenerarCartaHTML } from "./funciones";
+import {
+  GenerarValorCarta,
+  GenerarCartaHTML,
+  ConseguirValorCarta,
+  ConsultarPuntuaje,
+  DesactivasBotones,
+  ElegirGanador,
+  botonParar,
+  botonPedir,
+  ConvertirStringANumber
+} from "./funciones";
 
 window.onload = function() {
   let valoresCartas = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"],
-    cartasJugador = [],
-    cartasComputadora = [],
     simbolos = ["♦", "♥", "♠", "♣"],
-    simbolosEspeciales = ["K", "Q"];
+    NumCartaHtmlJugador = 1,
+    NumCartaHtmlRival = 2,
+    CartasJugador = [],
+    CartasRival = [],
+    PrimeraCartaRival = GenerarValorCarta(valoresCartas, simbolos);
 
-  GenerarCartaHTML(GenerarValorCarta(valoresCartas, simbolos));
+  // Generamos la primera carta
+
+  GenerarCartaHTML(PrimeraCartaRival, `computadora-carta-1`);
+  CartasRival.push(`${ConseguirValorCarta(PrimeraCartaRival)}`);
+
+  // constantes con la referencia para html
+  const PuntuajeJugaHTML = document.querySelector(".puntuaje-j"),
+    PuntuajeRivaHTML = document.querySelector(".puntuaje-r");
+
+  // Boton para pedir carta
+  botonPedir.addEventListener("click", () => {
+    // generamos los valores de las cartas
+    let cartaJugador = GenerarValorCarta(valoresCartas, simbolos);
+    let cartaRival = GenerarValorCarta(valoresCartas, simbolos);
+
+    // generamos el html de cada carta
+    GenerarCartaHTML(cartaJugador, `carta-${NumCartaHtmlJugador}`);
+    GenerarCartaHTML(cartaRival, `computadora-carta-${NumCartaHtmlRival}`);
+
+    // insertamos los valores en un array para consultar el puntuaje
+    CartasJugador.push(`${ConseguirValorCarta(cartaJugador)}`);
+    console.log(CartasJugador);
+    CartasRival.push(`${ConseguirValorCarta(cartaRival)}`);
+    console.log(CartasRival);
+
+    // sumamos uno para que el htlm tome como referencia la carta de al lado
+    NumCartaHtmlJugador += 1;
+    NumCartaHtmlRival += 1;
+
+    PuntuajeJugaHTML.textContent = `${ConsultarPuntuaje(CartasJugador)}`;
+    PuntuajeRivaHTML.textContent = `${ConsultarPuntuaje(CartasRival)}`;
+
+    ElegirGanador(
+      ConvertirStringANumber(PuntuajeJugaHTML.textContent),
+      ConvertirStringANumber(PuntuajeRivaHTML.textContent)
+    );
+  });
 };
--- src/funciones.js
+++ src/funciones.js
@@ -1,10 +1,20 @@
 import { arrow } from "@popperjs/core";
 
+export const botonPedir = document.querySelector(".btn-pedir"),
+  botonParar = document.querySelector(".btn-parar"),
+  botonReiniciar = document.querySelector(".btn-reiniciar");
+
+// numero aleatorio
+export const ConvertirStringANumber = string => {
+  return parseInt(string, 10);
+};
+
 const NumeroAleatorio = arr => {
   let randomNumber = Math.floor(Math.random() * arr.length);
   return randomNumber;
 };
 
+// generar el valor y el simbolo de la carta
 export const GenerarValorCarta = (valores, simbolos) => {
   let valorAleatorio = valores[NumeroAleatorio(valores)];
 
@@ -18,28 +28,72 @@ export const GenerarValorCarta = (valores, simbolos) => {
   return carta;
 };
 
-export const GenerarCartaHTML = arr => {
-  const cartas = document.querySelectorAll(".carta");
-
-  cartas.forEach(function(carta) {
-    carta.innerHTML = ` <div class="row">
+//introducir carta html junto los dos valores de la funcion de generar valor junto a la clase de la carta
+export const GenerarCartaHTML = (ValoresCarta, numcarta) => {
+  const carta = document.querySelector(`.${numcarta}`);
+  carta.innerHTML = ` <div class="row">
                 <div
                   class="col-12 contenedor-carta rounded-3 bg-white text-danger"
                 >
                   <div class="row">
                     <div class="col-12">
-                      <span>♣</span>
+                      <span class="fw-bold">${ValoresCarta[1]}</span>
                     </div>
                   </div>
                   <div class="row contenedorNumero">
-                    <div class="col-12">8</div>
+                    <div class="col-12">${ValoresCarta[0]}</div>
                   </div>
                   <div class="row">
                     <div class="col-12 icono-abajo">
-                      <span>♣</span>
+                      <span class="fw-bold">${ValoresCarta[1]}</span>
                     </div>
                   </div>
                 </div>
               </div>`;
-  });
+};
+
+// funcion para sacar el valor de cada carta
+export const ConseguirValorCarta = arr => {
+  if (arr[0] > 0) {
+    return arr[0];
+  } else if (arr[0] === "K") {
+    return 10;
+  } else if (arr[0] === "J") {
+    return 10;
+  } else if (arr[0] === "Q") {
+    return 10;
+  } else {
+    return "A";
+  }
+};
+
+export const DesactivasBotones = (btnParar, botonPedir) => {
+  btnParar.disabled = true;
+  botonPedir.disabled = true;
+};
+
+export const ElegirGanador = (puntuajeJugador, puntuajeRival) => {
+  if (puntuajeJugador > 21) {
+    alert("Has superado de 21");
+    DesactivasBotones(botonParar, botonPedir);
+  } else if (puntuajeRival > 21) {
+    alert("Has ganado la computadora se ha pasado");
+    DesactivasBotones(botonParar, botonPedir);
+  } else if (puntuajeJugador === 21 && puntuajeRival === 21) {
+    alert("Empate");
+    DesactivasBotones(botonParar, botonPedir);
+  }
+};
+
+export const ConsultarPuntuaje = arr => {
+  let letra = "";
+  let puntuaje = 0;
+  for (let i = 0; i < arr.length; i++) {
+    let tipo =
+      arr[i] === "A"
+        ? (letra = arr[i] + letra)
+        : (puntuaje += ConvertirStringANumber(arr[i]));
+  }
+
+  return puntuaje;
 };
